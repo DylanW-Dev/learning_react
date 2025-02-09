2d440432b83fbd024b78d08485e2c7b14eba4528
DylanW-Dev <dylan.wanten.dev@gmail.com>
2025-02-09 23:25:21 +0100

    Revert "Added function that display the date and time"
    This reverts commit 11b9719652496473747ebee268b59bb1206e14ef.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { useState, useEffect } from 'react';
 
-export default function App() {
-  const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date()); // Update time every second
-    }, 1000);
-
-    return () => clearInterval(interval); // Cleanup on unmount
-  }, []);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div style={{ textAlign: 'center', fontSize: '24px', marginTop: '20px' }}>
-      <p>Today is the {time.toLocaleDateString()}</p>
-      <p>and the Current Time is</p>
-      <strong>{time.toLocaleTimeString()}</strong>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
