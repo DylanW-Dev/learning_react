4d4c8ad54c29331515c6aac7d6beb61edd9b9d14
DylanW-Dev <dylan.wanten.dev@gmail.com>
2025-02-10 01:30:41 +0100

    Add a second component

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class TestComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is a second component</h1>
+            </div>
+        );
+    }
+}
+
+export default TestComponent;
